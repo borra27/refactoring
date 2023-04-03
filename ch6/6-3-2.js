4b449ef3ecb55bcbff9e4119911bcca278420c78
borra <ryle2724@gmail.com>
2023-04-03 20:42:37 +0900

    ch 6-3 실습

--- ch6/6-3-2.js
+++ ch6/6-3-2.js
@@ -1,8 +1,28 @@
-export function price(aRecord) {
-  const { quantity, itemPrice } = aRecord;
-  return (
-    quantity * itemPrice -
-    Math.max(0, quantity - 500) * itemPrice * 0.05 +
-    Math.min(quantity * itemPrice * 0.1, 100)
-  );
+export class Order {
+  #data;
+
+  constructor(aRecord) {
+    this.#data = aRecord;
+  }
+
+  get quantity() {
+    return this.#data.quantity;
+  }
+  get itemPrice() {
+    return this.#data.itemPrice;
+  }
+
+  get price() {
+    return this.basePrice - this.discount + this.shipping;
+  }
+
+  get basePrice() {
+    return this.quantity * this.itemPrice;
+  }
+  get discount() {
+    return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;
+  }
+  get shipping() {
+    return Math.min(this.quantity * this.itemPrice * 0.1, 100);
+  }
 }

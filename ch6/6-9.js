aaea829702b47341ea636158e2c3460ba652c36e
borra <ryle2724@gmail.com>
2023-04-15 23:33:56 +0900

    ch 6-9 예습

--- ch6/6-9.js
+++ ch6/6-9.js
@@ -1,10 +1,52 @@
-const reading = { customer: 'ivan', quantity: 10, month: 5, year: 2017 };
+class Reading {
+  #customer;
+  #quantity;
+  #month;
+  #year;
+  constructor(data) {
+    this.#customer = data.customer;
+    this.#quantity = data.quantity;
+    this.#month = data.month;
+    this.#year = data.year;
+  }
+
+  get customer() {
+    return this.#customer;
+  }
+  get quantity() {
+    return this.#quantity;
+  }
+  get month() {
+    return this.#month;
+  }
+  get year() {
+    return this.#year;
+  }
+
+  baseRate() {
+    if (this.#year === 2017 && this.#month === 5) return 0.1;
+    return 0.2;
+  }
+
+  taxThreshold(year) {
+    return 0.1;
+  }
+  taxableCharge() {
+    return Math.max(0, this.baseRate() - taxThreshold(this.#year));
+  }
+
+  baseCharge() {
+    return this.baseRate() * this.#quantity;
+  }
+}
+
+const reading = new Reading({
+  customer: "ivan",
+  quantity: 10,
+  month: 5,
+  year: 2017,
+});
 
 export function acquireReading() {
   return reading;
-}
-
-export function baseRate(month, year) {
-  if (year === 2017 && month === 5) return 0.1;
-  return 0.2;
 }

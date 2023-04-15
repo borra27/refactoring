bea62c9ccd014ff724a0e5077a2252b557d2536e
borra <ryle2724@gmail.com>
2023-04-15 18:52:01 +0900

    ch 6-8 예습

--- ch6/6-8.js
+++ ch6/6-8.js
@@ -1,15 +1,15 @@
-export function readingsOutsideRange(station, min, max) {
-  return station.readings.filter((r) => r.temp < min || r.temp > max);
+export function readingsOutsideRange(readings, range) {
+  return readings.filter((r) => r.temp < range.min || r.temp > range.max);
 }
 
 const station = {
-  name: 'ZB1',
+  name: "ZB1",
   readings: [
-    { temp: 47, time: '2016-11-10 09:10' },
-    { temp: 53, time: '2016-11-10 09:20' },
-    { temp: 58, time: '2016-11-10 09:30' },
-    { temp: 53, time: '2016-11-10 09:40' },
-    { temp: 51, time: '2016-11-10 09:50' },
+    { temp: 47, time: "2016-11-10 09:10" },
+    { temp: 53, time: "2016-11-10 09:20" },
+    { temp: 58, time: "2016-11-10 09:30" },
+    { temp: 53, time: "2016-11-10 09:40" },
+    { temp: 51, time: "2016-11-10 09:50" },
   ],
 };
 const operationPlan = {
@@ -17,8 +17,7 @@ const operationPlan = {
   temperatureCeiling: 53,
 };
 
-readingsOutsideRange(
-  station,
-  operationPlan.temperatureFloor,
-  operationPlan.temperatureCeiling
-);
+readingsOutsideRange(station.readings, {
+  min: operationPlan.temperatureFloor,
+  max: operationPlan.temperatureCeiling,
+});

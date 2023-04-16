3bd4c078ddf86b00b77e8c5ef3c332d2558e689f
borra <ryle2724@gmail.com>
2023-04-16 21:41:12 +0900

    ch 6-11-2 예습

--- ch6/6-11-2.js
+++ ch6/6-11-2.js
@@ -1,18 +1,31 @@
-import fs from 'fs';
+import fs from "fs";
 
-if (!process.argv[2]) {
-  throw new Error('파일 이름을 입력하세요');
+const fileName = getFile(process.argv);
+const orders = getOrders(fileName);
+checkReady(process.argv, orders);
+
+function getFile(argv) {
+  if (!argv[2]) {
+    throw new Error("파일 이름을 입력하세요");
+  }
+
+  const fileName = `./${process.argv[2]}.json`;
+  if (!fs.existsSync(fileName)) {
+    throw new Error("파일이 존재하지 않습니다");
+  }
+
+  return fileName;
 }
 
-const fileName = `./${process.argv[2]}.json`;
-if (!fs.existsSync(fileName)) {
-  throw new Error('파일이 존재하지 않습니다');
+function getOrders(file) {
+  const rawData = fs.readFileSync(file);
+  return JSON.parse(rawData);
 }
 
-const rawData = fs.readFileSync(fileName);
-const orders = JSON.parse(rawData);
-if (process.argv.includes('-r')) {
-  console.log(orders.filter((order) => order.status === 'ready').length);
-} else {
-  console.log(orders.length);
+function checkReady(argv, orders) {
+  if (argv.includes("-r")) {
+    console.log(orders.filter((order) => order.status === "ready").length);
+  } else {
+    console.log(orders.length);
+  }
 }

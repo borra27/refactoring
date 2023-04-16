b6092e7f0e5f78addb459ee5421155add586fe9a
borra <ryle2724@gmail.com>
2023-04-16 21:49:00 +0900

    ch 6-11-2 실습

--- ch6/6-11-2.js
+++ ch6/6-11-2.js
@@ -1,31 +1,35 @@
 import fs from "fs";
 
-const fileName = getFile(process.argv);
-const orders = getOrders(fileName);
-checkReady(process.argv, orders);
+// 1. run 함수를 만들어서 노드의 process 디펜던시를 제거함
+run(process.argv);
 
-function getFile(argv) {
-  if (!argv[2]) {
+// 2. 사용자에게 입력을 받아옴 -> 유효성 검사
+// 3. 필요한 로직 처리
+function run(args) {
+  countOrders(parseCommand(args));
+}
+
+function parseCommand(args) {
+  if (!args[2]) {
     throw new Error("파일 이름을 입력하세요");
   }
 
-  const fileName = `./${process.argv[2]}.json`;
+  const fileName = `./${args[2]}.json`;
   if (!fs.existsSync(fileName)) {
     throw new Error("파일이 존재하지 않습니다");
   }
 
-  return fileName;
+  return {
+    fileName,
+    countReadyOnly: args.includes("-r"),
+  };
 }
 
-function getOrders(file) {
-  const rawData = fs.readFileSync(file);
-  return JSON.parse(rawData);
-}
-
-function checkReady(argv, orders) {
-  if (argv.includes("-r")) {
-    console.log(orders.filter((order) => order.status === "ready").length);
-  } else {
-    console.log(orders.length);
-  }
+function countOrders({ fileName, countReadyOnly }) {
+  const rawData = fs.readFileSync(fileName);
+  const orders = JSON.parse(rawData);
+  const filtered = countReadyOnly
+    ? orders.filter((order) => order.status === "ready")
+    : orders;
+  console.log(filtered.length);
 }

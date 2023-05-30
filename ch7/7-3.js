a984079ccd91571bfba6a004dc21d39c7b01463b
borra <ryle2724@gmail.com>
2023-05-30 11:00:18 +0900

    ch 9 실습

--- ch7/7-3.js
+++ ch7/7-3.js
@@ -2,14 +2,20 @@ export class Order {
   constructor(data) {
     this.priority = data.priority;
   }
+
+  getHighPriorityCount() {
+    return this.orders.filter(
+      (o) => "high" === o.priority || "rush" === o.priority
+    ).length;
+  }
 }
 
 const orders = [
-  new Order({ priority: 'normal' }),
-  new Order({ priority: 'high' }),
-  new Order({ priority: 'rush' }),
+  new Order({ priority: "normal" }),
+  new Order({ priority: "high" }),
+  new Order({ priority: "rush" }),
 ];
 
 const highPriorityCount = orders.filter(
-  (o) => 'high' === o.priority || 'rush' === o.priority
+  (o) => "high" === o.priority || "rush" === o.priority
 ).length;

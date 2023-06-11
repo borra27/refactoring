ab1d86fa0660091edb57e67b771ef7de1a00739e
borra <ryle2724@gmail.com>
2023-06-11 21:45:21 +0900

    ch 10-1 예습

--- ch10/10-1.js
+++ ch10/10-1.js
@@ -1,7 +1,5 @@
 function calculateCharge(date, quantity, plan) {
-  let charge = 0;
   if (!date.isBefore(plan.summerStart) && !date.isAfter(plan.summerEnd))
-    charge = quantity * plan.summerRate;
-  else charge = quantity * plan.regularRate + plan.regularServiceCharge;
-  return charge;
+    return quantity * plan.summerRate;
+  return quantity * plan.regularRate + plan.regularServiceCharge;
 }

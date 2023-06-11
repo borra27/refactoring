e6e4e1413d7162b35d9f45946b31c428e2dd7b32
borra <ryle2724@gmail.com>
2023-06-11 21:59:23 +0900

    ch 10-3 예습

--- ch10/10-3-1.js
+++ ch10/10-3-1.js
@@ -1,22 +1,11 @@
 export function payAmount(employee) {
-  let result;
   if (employee.isSeparated) {
-    result = { amount: 0, reasonCode: 'SEP' };
+    return { amount: 0, reasonCode: "SEP" };
+  } else if (employee.isRetired) {
+    return { amount: 0, reasonCode: "RET" };
+  } else if (!employee.isRetired) {
+    return { amount: 999, reasonCode: "UNICORN" };
   } else {
-    if (employee.isRetired) {
-      result = { amount: 0, reasonCode: 'RET' };
-    } else {
-      // lorem.ipsum(dolor.sitAmet);
-      // consectetur(adipiscing).elit();
-      // sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-      // ut.enim.ad(minim.veniam);
-      result = someFinalComputation();
-      return result;
-    }
+    return;
   }
-  return result;
-}
-
-function someFinalComputation() {
-  return { amount: 999, reasonCode: 'UNICORN' };
 }
--- ch10/10-3-2.js
+++ ch10/10-3-2.js
@@ -1,11 +1,12 @@
 export function adjustedCapital(instrument) {
-  let result = 0;
-  if (instrument.capital > 0) {
-    if (instrument.interestRate > 0 && instrument.duration > 0) {
-      result =
-        (instrument.income / instrument.duration) *
-        anInstrument.adjustmentFactor;
-    }
+  if (
+    instrument.capital > 0 &&
+    instrument.interestRate > 0 &&
+    instrument.duration > 0
+  ) {
+    return (
+      (instrument.income / instrument.duration) * anInstrument.adjustmentFactor
+    );
   }
-  return result;
+  return 0;
 }

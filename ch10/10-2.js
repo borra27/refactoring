4ec0ffd5c9c29318fc1fbde36ee9fb5f3060f34a
borra <ryle2724@gmail.com>
2023-06-11 21:52:57 +0900

    ch 10-2 실습

--- ch10/10-2.js
+++ ch10/10-2.js
@@ -1,9 +1,12 @@
 function disabilityAmount(employee) {
-  if (
+  if (isNotEligibleForDisability(employee)) return 0;
+  return 1;
+}
+
+function isNotEligibleForDisability(employee) {
+  return (
     employee.seniority < 2 ||
     employee.monthsDisabled > 12 ||
     employee.isPartTime
-  )
-    return 0;
-  return 1;
+  );
 }
